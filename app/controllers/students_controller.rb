7817c6441fad50460d3af2de83289dab69feef38
febbraiod <donedesign1@gmail.com>
2016-02-13 19:39:32 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class StudentsController < ApplicationController
   
   def index
@@ -12,6 +14,9 @@ class StudentsController < ApplicationController
   end
 
   def create
+    binding.pry
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to student_path(@student)
   end
 
 end
