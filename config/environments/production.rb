a3213be0c62cf06ae73fe72912eb673a7224ee88
jj54321 <mailme711@gmail.com>
2016-03-13 16:03:59 -0400

    Done.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+   config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
