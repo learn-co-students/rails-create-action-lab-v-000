78908e5e36006e3bf94c3f006b5cfd94ea05a233
Lady Codington <myemailforcoding@gmail.com>
2016-04-02 21:48:08 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -9,9 +9,20 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to student_path(@student)
+    # redirect_to new_student_path(student: params[:student])
+  end
+
+  def valid_parameters?
+    !params[:student][:first_name].empty? && !params[:student][:last_name].empty?
   end
 
 end
