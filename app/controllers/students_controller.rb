39963676c52f1a4ba537d503f5503e3a16ae5f8e
smithWEBtek <brad@smithwebtek.com>
2016-05-18 18:51:21 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,11 @@ class StudentsController < ApplicationController
   end
 
   def new
+    render 'students/new'
   end
 
   def create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to "/student/#{@student.id}"
   end
-
 end
