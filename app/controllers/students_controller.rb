60b6a1aa31be2bcf600194f86e8fff2af24ec684
Spencer Evans <spencevans@gmail.com>
2017-08-01 18:06:07 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,14 +10,11 @@ class StudentsController < ApplicationController
   end
 
   def new
-    @student = Student.new
+
   end
 
   def create
-    @student = Student.new
-    #binding.pry
-    @student.first_name = params[:first_name]
-    @student.last_name = params[:last_name]
+    @student = Student.new(first_name: params[:first_name], last_name: params[:last_name])
     @student.save
     redirect_to student_path(@student)
   end
