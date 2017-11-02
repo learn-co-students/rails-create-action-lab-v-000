79bdac6f311c31b8bd89f01374012a04f4997e34
github username <github email address>
2017-11-02 12:51:18 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -8,10 +8,12 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def new
+  def new # implicitly renders the views/students/new.html.erb view file, which presents the form to create a new student
   end
 
-  def create
+  def create # we will NOT render a create.html.erb view file. Instead, we WILL redirect to the show page of the @student instance just created in this method.
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to student_path(@student)
   end
 
 end
