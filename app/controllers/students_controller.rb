91f15f95acfa171f5ba50bfa57f00ea5d2ec4c74
David Towster <djt4xe@virginia.edu>
2018-02-27 10:48:40 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class StudentsController < ApplicationController
   
   def index
@@ -12,6 +14,8 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to student_path(@student)
   end
 
 end
