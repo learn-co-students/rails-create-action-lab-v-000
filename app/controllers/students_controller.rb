7265c1d8d2dbcd1668fe684e8b5c40bdf32ca327
ElvinGarcia <dimix181@gmail.com>
2018-05-18 22:27:20 -0400

    refractor controller to include private methods

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,11 @@
 class StudentsController < ApplicationController
-
+  before_action :set_student, only: :show
+  before_action :all_students, only: :index
   def index
-    @students = Student.all
+  #  @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
   def new
@@ -15,6 +15,17 @@ class StudentsController < ApplicationController
     @student = Student.new( first_name:params[:first_name], last_name:params[:last_name])
     @student.save
     redirect_to student_path(@student)
+  end
+
+
+  private
+
+  def all_students
+    @students = Student.all
+  end
+
+  def set_student
+      @student = Student.find(params[:id])
   end
 
 end
