1e5add1a5d58c5a43cce7cd9ce19ebf2934b03a5
AlexFriedland <friedlandaj@gmail.com>
2018-05-20 19:08:34 +0200

    setup db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create!(first_name: "Alex", last_name: "Friedland")
+Student.create!(first_name: "Harrison", last_name: "Friedland")
+Student.create!(first_name: "Milica", last_name: "Medojevich")
