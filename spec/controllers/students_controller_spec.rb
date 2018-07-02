f1646a3374c2c0ec3b47f04c429665bdcfc115ce
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-07-02 11:39:25 -0400

    Done.

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,20 +1,18 @@
-require 'rails_helper'
+class StudentsController < ApplicationController
 
-describe StudentsController do
-
-  describe "POST create" do
-    context "with valid attributes" do
-      it "creates a new student" do
-        expect{
-          post :create, { :first_name => "Sam", :last_name => "Smith" }
-        }.to change(Student,:count).by(1)
-      end
-
-      it "redirects to the new student" do
-        post :create, { :first_name => "Sam", :last_name => "Smith" }
-        expect(response).to redirect_to Student.last
-      end
-    end
+  def index
+    @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @student = Student.create({first_name: params[:first_name],last_name: params[:last_name]})
+    redirect_to student_path(@student)
+  end
 end
