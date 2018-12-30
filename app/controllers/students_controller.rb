f233f9621702373bf638ea30955de574ece32438
Judy McGoldrick <jjeanius@verizon.net>
2018-12-30 21:14:24 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class StudentsController < ApplicationController
 
   def index
@@ -9,11 +11,18 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
-    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
-    redirect_to @student
+    # @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to student_url(@student)
   end
+
+
 
 end
